003fd9abace5b2cc11216ac9d66da0b3cc7187d3
wkendnen-sketch <wkendnen@gmail.com>
2026-05-20 16:24:12 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ WEATHER_CAPTURE_1 = {
     "clip": {
         "x": 350,
         "y": 200,
-        "width": 800,
+        "width": 780,
         "height": 428,
     }
 }
@@ -78,7 +78,7 @@ WEATHER_CAPTURE_2 = {
     "clip": {
         "x": 350,
         "y": 399,
-        "width": 800,
+        "width": 780,
         "height": 290,
     }
 }
